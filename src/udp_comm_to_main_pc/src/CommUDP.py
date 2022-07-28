d96c42a3d1274fff9c8ab27c53c12b93008251c0
azidanit <azidan.it@gmail.com>
2022-07-28 10:09:14 -0400

    dummy callback

--- src/udp_comm_to_main_pc/src/CommUDP.py
+++ src/udp_comm_to_main_pc/src/CommUDP.py
@@ -107,6 +107,16 @@ class CommUDP:
         nav_sat_msg.longitude = long_
 
         self.publish_pos.publish(nav_sat_msg)
+
+        if float(dist_) <= 3:
+            msg_int32 = Int32MultiArray()
+            msg_int32.data.append(2)
+            msg_int32.data.append(1)
+            self.state_terminal_pub.publish(msg_int32)
+            msg_int32 = Int32MultiArray()
+            msg_int32.data.append(1)
+            msg_int32.data.append(4)
+            self.state_terminal_pub.publish(msg_int32)
         pass
 
     def sendUDPThread(self):
